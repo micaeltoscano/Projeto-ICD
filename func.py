e9a4883593cd5e991f1d5aae3d842343501ca8dc
micaeltoscano <micael.toscano@gmail.com>
2024-10-14 20:40:15 -0300

    enviando inicio do projeto final

--- func.py
+++ func.py
@@ -31,6 +31,58 @@ def alterar_type(dataset):
     for col in colunas_frequencia:
         dataset[col] = dataset[col].map(mapeamento_frequencia).astype(float)
 
+def limpeza(df):
+    media_idade = df["Age"].mean()
+    df["Age"] = df["Age"].fillna(media_idade)
+    
+    moda_streaming = df['Primary streaming service'].mode()[0]
+    df['Primary streaming service'] = df['Primary streaming service'].fillna(moda_streaming)
+    
+    moda_ww = df['While working'].mode()[0]
+    df['While working'] = df['While working'].fillna(moda_ww)
+    
+    moda_instru = df['Instrumentalist'].mode()[0]
+    df['Instrumentalist'] = df['Instrumentalist'].fillna(moda_instru)
+    
+    moda_Composer = df['Composer'].mode()[0]
+    df['Composer'] = df['Composer'].fillna(moda_Composer)
+    
+    moda_Foreign_languages = df['Foreign languages'].mode()[0]
+    df['Foreign languages'] = df['Foreign languages'].fillna(moda_Foreign_languages)
+    
+    mediana_bpm = df['BPM'].median()
+    df.loc[df["BPM"] > 300, "BPM"] = mediana_bpm
+    
+    media_bpm = df['BPM'].mean()
+    df["BPM"] = df["BPM"].fillna(media_bpm)
+    
+    mode_ME = df["Music effects"].mode()[0]
+    df["Music effects"] = df["Music effects"].fillna(mode_ME)
+
+def traducao(df):
+    colunas_traduzidas = [
+    "data_e_hora_envio", "idade", "servico_de_streaming_principal",
+    "horas_por_dia", "enquanto_trabalha",
+    "instrumentalista", "compositor", "genero_fav",
+    "exploratorio", "em_outros_idiomas", "bpm",
+    "classica", "country",
+    "edm", "folk", "gospel",
+    "hip_hop", "jazz", "kpop",
+    "latin", "lofi", "metal",
+    "pop", "rb", "rap",
+    "rock", "musica_videogame",
+    "ansiedade", "depressao", "insonia", "toc",
+    "efeitos_na_saude_mental", "permissoes"
+]
+
+    legenda = """
+    0 - Nunca
+    1 - Raramente
+    2 - Algumas vezes
+    3 - Bem frequentemente """
+
+    df.columns = colunas_traduzidas
+    
 def exibir_boxplot(dataset, coluna, orientacao, titulo):
 
     #Função para exibir os bloxplots. ds = dataset, coluna = nome da coluna, orientação = "h" para horizontal e "v" para vertical, titulo = nome do titulo. 
@@ -57,3 +109,19 @@ def boxplot_de_varias(dataset, titulo, tamanho_x, tamanho_y, legenda, *colunas):
     plt.title(titulo)
     plt.figtext(0.93, 0.5, legenda, va='center', ha='left', fontsize=10)
     plt.show()
+
+def grafico_barra(df, coluna, titulo, x_label, y_label, legenda ):
+    plt.figure(figsize=(12, 6))
+    df.set_index(coluna).plot(kind='bar', stacked=False, alpha=0.75)
+    
+    # Adicionando título e rótulos
+    plt.title(titulo)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xticks(rotation=45)  # Rotaciona os rótulos do eixo x
+    plt.legend(title= legenda)
+    
+    plt.show()
+
+
+
